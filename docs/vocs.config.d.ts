e37e041036ad8113811523e9b8519d58edcaf077
dsavelyev <kriper0nind@gmail.com>
2025-07-31 10:11:06 +0300

    docs

---
+++ docs/vocs.config.d.ts
@@ -0,0 +1,2 @@
+declare const _default: Promise<import("vocs/_lib/config").ParsedConfig>;
+export default _default;
--- docs/vocs.config.ts
+++ docs/vocs.config.ts
@@ -8,6 +8,10 @@ export default defineConfig({
       link: '/getting-started',
     },
     {
+      text: 'Touch configuration',
+      link: '/working-with-config',
+    },
+    {
       text: 'addPeer',
       link: '/add-peer',
     },
